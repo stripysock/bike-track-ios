8fede009da43327e7f8c8088c5a49e642de37560
adamcoop <adam@stripysock.com.au>
2023-08-25 12:12:55 +1000

    Initial Commit

---
+++ BikeTrack/BikeTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeTrackApp.swift
+//  BikeTrack
+//
+//  Created by Adam Cooper on 25/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct BikeTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
