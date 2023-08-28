d01b83e15a7db2cec8de14f12be3ac58887d01b2
adamcoop <adam@stripysock.com.au>
2023-08-28 14:07:40 +1000

    Add UI Test for sign up password validation

--- BikeTrackUITests/BikeTrackUITests.swift
+++ BikeTrackUITests/BikeTrackUITests.swift
@@ -9,25 +9,46 @@ import XCTest
 
 final class BikeTrackUITests: XCTestCase {
 
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSignUpAppearance() throws {
+        app.buttons["Create Account"].tap()
+        
+        let signInMessage = app.staticTexts["Sign up for an account."]
+        XCTAssert(signInMessage.waitForExistence(timeout: 0.5))
+    }
+    
+    func testSignUpFormValidation() throws {
+        try testSignUpAppearance()
+        
+        let emailField = app.textFields["Email"]
+        let passwordField = app.secureTextFields["Password"]
+        let submitField = app.buttons["Sign Up"]
+        
+        XCTAssert(emailField.waitForExistence(timeout: 0.5))
+        XCTAssert(passwordField.waitForExistence(timeout: 0.5))
+        XCTAssert(submitField.waitForExistence(timeout: 0.5))
+        
+        emailField.tap()
+        emailField.typeText("adam@stripysock.com.au")
+        XCTAssert(!submitField.isEnabled)
+        
+        passwordField.tap()
+        passwordField.typeText("password")
+        XCTAssert(submitField.isEnabled)
     }
 
     func testLaunchPerformance() throws {
