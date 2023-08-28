678ea7fea8808e90c0ef6ffa501a2437768fe3b8
adamcoop <adam@stripysock.com.au>
2023-08-28 11:33:47 +1000

    Add user creation test

--- BikeTrackTests/BikeTrackTests.swift
+++ BikeTrackTests/BikeTrackTests.swift
@@ -2,7 +2,20 @@ import XCTest
 @testable import BikeTrack
 
 final class BikeTrackTests: XCTestCase {
+    let mockUserService = UserService(authRepository: MockAuthRepository())
     
-    
+    func testMockAccountCreation() async {
+        let expectation = XCTestExpectation(description: "User created successfully")
+        
+        let emailAddress = "adam@stripysock.com.au"
+        let password = "password1234"
+        
+        do {
+            try await mockUserService.signUp(emailAddress: emailAddress, password: password)
+            expectation.fulfill()
+        } catch {
+            XCTFail("Unable to create user.")
+        }
+    }
 
 }
